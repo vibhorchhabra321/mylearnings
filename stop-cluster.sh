3a08e239f11bfc6f524d2566259a5a0523744732
vibhor <vibhor.rc@gmail.com>
2020-01-09 23:32:51 +0530

    Adding a cluster stop script using a resouce-group

---
+++ stop-cluster.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+  aws ec2 stop-instances --instance-ids $(aws resource-groups list-group-resources --group-name $1 | grep ResourceArn | cut -d "/" -f 2 | tr '\n' ' ' | sed -r 's/\",/ /g')  
